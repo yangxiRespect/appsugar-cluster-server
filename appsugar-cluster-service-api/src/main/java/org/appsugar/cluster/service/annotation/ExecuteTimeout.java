6f3781084725229acc0eed72a942bf8cd670f9a1
wangqiang <wangqiang@ggxueche.com>
2018-01-16 18:19:56 +0800

    fix timeout bug

--- appsugar-cluster-service-api/src/main/java/org/appsugar/cluster/service/annotation/ExecuteTimeout.java
+++ appsugar-cluster-service-api/src/main/java/org/appsugar/cluster/service/annotation/ExecuteTimeout.java
@@ -1,10 +1,18 @@
 package org.appsugar.cluster.service.annotation;
 
+import static java.lang.annotation.ElementType.METHOD;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * 调用超时设置
  * @author NewYoung
  * 2016年7月1日上午10:10:11
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = { METHOD })
 public @interface ExecuteTimeout {
 	long value() default 30000;
 }
